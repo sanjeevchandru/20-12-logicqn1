d5e24831689dd77cd907864521cf9aa1c465098c
sanjeevchandru <chandrusanjeev5@gmail.com>
2023-12-20 19:27:12 +0530

    20-12-logicqtn1

---
+++ main.js
@@ -0,0 +1,12 @@
+let aa=12,b=8,c=-5;
+document.write("Find greatest number"+"<br>")
+<document.write("The number are " +aa +", "+b+", "+c+"<br>")
+if(aa>b && aa>c){
+    document.write(aa+ " is greater")
+}
+else if(b>aa && b>c){
+    document.write(b+" is greater")
+}
+else{
+    document.write(c+" is greater");
+}
